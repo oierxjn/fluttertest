875c4b6237d4d6bc32adc70ec9467745ceae6c77
oierxjn <1934672944@qq.com>
2026-01-16 01:43:18 +0800

    feat: 添加猜词输入框并重构游戏页面为有状态组件
    - 将 GamePage 从 StatelessWidget 改为 StatefulWidget 以支持状态管理
    - 新增 GuessInput 组件处理用户输入
    - 实现猜词提交功能并清空输入框

--- lib/main.dart
+++ lib/main.dart
@@ -39,9 +39,14 @@ class Tile extends StatelessWidget {
 
 }
 
-class GamePage extends StatelessWidget {
-  GamePage({super.key});
+class GamePage extends StatefulWidget {
+  const GamePage({super.key});
 
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
   final Game _game = Game();
 
   @override
@@ -54,13 +59,68 @@ class GamePage extends StatelessWidget {
           for (var guess in _game.guesses)
             Row(
               spacing: 5,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 for (var letter in guess)
                   Tile(letter.char, letter.type),
               ],
-            )
+            ),
+          GuessInput(
+            onSubmitGuess: (String guess) {
+              setState(() {
+                _game.guess(guess);
+              });
+            },
+          ),
         ],
       )
+    );
+  }
+}
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+
+  final TextEditingController _textEditingController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
+  void _onSubmit(){
+    onSubmitGuess(_textEditingController.text.trim());
+    _textEditingController.clear();
+    _focusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+              controller: _textEditingController,
+              autofocus: true,
+              focusNode: _focusNode,
+              onSubmitted: (value) {
+                _onSubmit();
+              },
+            ),
+          )
+        ),
+        IconButton(
+          padding: EdgeInsets.zero,
+          icon: Icon(Icons.arrow_circle_up),
+          onPressed: _onSubmit,
+        ),
+      ]
     );
   }
 }
