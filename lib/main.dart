b8f7c370b6a958bc5a201b5c97e270c70c40f115
oierxjn <1934672944@qq.com>
2026-01-16 08:29:45 +0800

    feat: 使用 AnimatedContainer 添加平铺动画效果
    为 Tile 组件添加平滑的动画过渡效果，提升用户体验

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class Tile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 500),
       width: 60,
       height: 60,
       decoration: BoxDecoration(
